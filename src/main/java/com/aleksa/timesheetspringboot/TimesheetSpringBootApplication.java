36fcc8b79839e071637fb6f29372dfc6c18efec7
aleksa001 <flatronw2@outlook.com>
2023-02-24 18:07:55 +0100

    Init

---
+++ src/main/java/com/aleksa/timesheetspringboot/TimesheetSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.aleksa.timesheetspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aleksa/timesheetspringboot/TimesheetSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksa.timesheetspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
